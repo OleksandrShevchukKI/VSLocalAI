e6054f926a8b9f974aed1d51298c3f7ade39d941
Oleksandr Shevchuk <oleksandr.shevchuk222@gmail.com>
2025-03-12 14:40:27 +0200

    Implemented L.AI.Options

---
+++ L.AI.Options/Base/GeneralProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace L_AI.Options.Base
+{
+    public class DialogPageProvider
+    {
+        [ComVisible(true)]
+        public class General : BaseOptionPage<GeneralOptions> { }
+    }
+}
---
+++ L.AI.Options/GenerationProviderType.cs
@@ -0,0 +1,8 @@
+﻿namespace L_AI.Options
+{
+    public enum GenerationProviderType
+    {
+        Kobold,
+        OogaBooga,
+    }
+}
