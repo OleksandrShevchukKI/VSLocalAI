e7cf473ed8ec66e28119044aae3bb887f64ce27b
Oleksandr Shevchuk <oleksandr.shevchuk222@gmail.com>
2025-03-12 14:42:34 +0200

    Added Manager and WebApi to L.AI.Options

---
+++ L.AI.TextGeneration/WebApi/ApiResult.cs
@@ -0,0 +1,30 @@
+﻿namespace L_AI.TextGeneration.WebApi
+{
+    public class ApiResult<T>
+    {
+        public bool IsSuccessful { get; set; }
+        public T Data { get; set; }
+        public string ErrorMessage { get; set; }
+
+
+        public static ApiResult<T> Result(T result)
+        {
+            return new ApiResult<T>
+            {
+                IsSuccessful = true,
+                ErrorMessage = null,
+                Data = result,
+            };
+        }
+
+        public static ApiResult<T> Error(string errorMessage)
+        {
+            return new ApiResult<T>
+            {
+                IsSuccessful = false,
+                ErrorMessage = errorMessage,
+                Data = default,
+            };
+        }
+    }
+}
---
+++ L.AI.TextGeneration/WebApi/GenerationRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace L_AI.TextGeneration.WebApi
+{
+    public class GenerationRequestModel
+    {
+        public int ContextLength { get; set; }
+        public string[] Stop { get; set; }
+        public string Prompt { get; set; }
+    }
+}
